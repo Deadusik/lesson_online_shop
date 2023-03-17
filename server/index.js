d2a80da4aba1d699eb0c3e63042cce465a8ea407
Vabalabadabdab <110040060+Deadusik@users.noreply.github.com>
2023-03-18 00:43:48 +0200

    add middleware
    add universal error handler
    add logic for controllers

--- server/index.js
+++ server/index.js
@@ -5,10 +5,15 @@ const sequelize = require('./db')
 const models = require('./models/models')
 const cors = require('cors')
 const router = require('./routes/index')
+const errorHandler = require('./middleware/ErrorHandlingMiddleware')
+const fileUpload = require('express-fileupload')
+const path = require('path')
 
 const app = express()
 app.use(cors())
+app.use(fileUpload({}))
 app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'static')))
 app.use('/api', router)
 
 app.get('/', (req, res) => {
@@ -16,6 +21,8 @@ app.get('/', (req, res) => {
         .status(200)
         .json({ message: 'working!' })
 })
+
+app.use(errorHandler)
 
 const start = async () => {
     try {
